c1d3cd2bba8000760e497f6a2fd3784427ca8229
Pedrohrlt <120110403+Pedrohrlt@users.noreply.github.com>
2025-11-13 15:58:53 -0300

    Change method visibility from protected to public

--- BibliotecaTests/src/main/java/com/example/demo/services/AbstractService.java
+++ BibliotecaTests/src/main/java/com/example/demo/services/AbstractService.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 public abstract class AbstractService<T, ID> implements GenericService<T, ID> {
 
-    protected <R> R checkNotNull(Optional<R> entity, String msg) {
+    public <R> R checkNotNull(Optional<R> entity, String msg) {
         return entity.orElseThrow(() -> new RuntimeException(msg));
     }
 
